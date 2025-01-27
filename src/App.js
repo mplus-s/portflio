b2b8bae3d4f05c2b70352152ab338d45cd5b7860
Muhammad Shahzad <muhammad.shahzad@selectquote.com>
2025-01-27 20:57:07 +0500

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css"
+import styled from "styled-components"
+import MainContainer from "./components/layout/main-container/MainContainer"
+
+const Background = styled.div`
+  background-color: aliceblue;
+  min-width: 100%;
+  min-height: 100vh;
+`
 
 function App() {
-  return <div>react app</div>
+  return (
+    <Background>
+      <MainContainer />
+      react app
+    </Background>
+  )
 }
 
 export default App
---
+++ src/components/layout/main-container/MainContainer.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from "styled-components"
+
+const Main = styled.div`
+  background-color: blue;
+  border-radius: 90px;
+  margin: 20vh 10% 0 15%;
+  max-width: 75%;
+  min-height: 60vh;
+`
+
+export default function MainContainer() {
+  return <Main>MainContainer</Main>
+}
