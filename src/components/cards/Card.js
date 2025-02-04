b6268557906015e5ce4805146121851859341411
Muhammad Shahzad <muhammad.shahzad@selectquote.com>
2025-02-04 22:55:52 +0500

    antd installed , card opttmizted to be reuse and grid basic level implimented

--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,23 +1,20 @@
-import React from "react"
-import styled from "styled-components"
+import React from 'react';
+import styled from 'styled-components';
 
 const StyledCard = styled.div`
-  background:rgb(76,76,78) ;
+  background: rgb(76, 76, 78);
   border: 1px solid white;
   border-radius: 20px;
-  width: ${(props) => props.width || "150px"};
-  height: ${(props) => props.height || "250px"};
-  margin: 10px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  padding: 15px;
-`
-export default function Card({ content, width, height }) {
+  width: ${(props) => props.width || '150px'};
+  height: ${(props) => props.height || '250px'};
+  margin: ${(props) => props.margin || '0px'};
+
+  padding: 5px;
+`;
+export default function Card(props) {
   return (
-    <StyledCard width={width} height={height}>
-      {content}
+    <StyledCard margin={props.margin} width={props.width} height={props.height}>
+      {props.children}
     </StyledCard>
-  )
+  );
 }
--- src/components/layout/main-container/MainContainer.js
+++ src/components/layout/main-container/MainContainer.js
@@ -1,7 +1,10 @@
-import React from "react"
-import styled from "styled-components"
-import Card from "./../../cards/Card"
+import React from 'react';
+import styled from 'styled-components';
+import Card from './../../cards/Card';
+import {Row, Col} from 'antd';
+
 const Main = styled.div`
+  position: relative;
   background: linear-gradient(135deg, #616066, #4f4e53);
   border: 1px solid white;
   border-radius: 90px;
@@ -14,13 +17,30 @@ const Main = styled.div`
   margin-left: 10px;
   flex-wrap: wrap;
   padding: 20px;
-`
-const attendenceCard = <h1> Attendence </h1>
+`;
 
 export default function MainContainer() {
   return (
     <Main>
-      <Card width="500px" height="500px" content={attendenceCard} />
+      <Card width="500px" height="500px">
+        <Row style={{backgroundColor: 'red'}}>
+          <Col style={{border: '1px solid black'}} span={5} offset={2}>
+            <p> col 1 </p>
+          </Col>
+          <Col style={{border: '1px solid black'}} span={5} offset={2}>
+            <p> col 2 </p>
+          </Col>
+          <Col style={{border: '1px solid black'}} span={6} offset={2}>
+            <p> col 3 </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col style={{backgroundColor: 'blue'}} span={24}>
+            {' '}
+            cols
+          </Col>
+        </Row>
+      </Card>
     </Main>
-  )
+  );
 }
